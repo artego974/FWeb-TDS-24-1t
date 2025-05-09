e60168de3e6176ecc4a08755bde4bd3cfe23bbeb
artego974 <artego974@gmail.com>
2025-05-09 14:04:14 -0300

    aulas que faltaram e mudulação da pokedex

--- aula 19/src/script.js
+++ aula 19/src/script.js
@@ -1,4 +1,4 @@
-let imgPK = document.querySelector("#fotoPK");
+let imgPK = document.querySelector("#fotoPK"); 
 let formPK = document.querySelector("#formPK");
 let inputPK = document.querySelector("#inputPK");
 
@@ -14,73 +14,87 @@ let alturaPK = document.querySelector("#alturaPK");
 
 let btnVoltar = document.querySelector("#btnVoltar");
 let btnProximo = document.querySelector("#btnProximo");
+let btnSom = document.querySelector("#btnSom");
 
 let numeroPokedex = 1;
 
-const fetchPokemon = async(pokemon)=>{
+
+let musicaFundo = new Audio("./assets/pokémon Theme Song.mp3");
+musicaFundo.loop = true;
+
+btnSom.addEventListener("click", () => {
+    if (musicaFundo.paused) {
+        musicaFundo.play();
+        btnSom.textContent = "Pausar Música";
+    } else {
+        musicaFundo.pause();
+        btnSom.textContent = "Música Pokémon";
+    }
+});
+
+const fetchPokemon = async (pokemon) => {
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await APIresponse.json();
-    return data
+    return data;
 }
-const showPokemon = async(pokemon) =>{
+
+const showPokemon = async (pokemon) => {
     const dataPokemon = await fetchPokemon(pokemon);
     const audio = new Audio(dataPokemon.cries.latest);
     audio.play();
- 
-try {
-    // Tenta definir a imagem padrão
-    if (dataPokemon.sprites.front_default) {
-      imgPK.src = dataPokemon.sprites.front_default;
-    } else if (dataPokemon.sprites.front_shiny) {
-      // Se não existir, tenta
-      imgPK.src = dataPokemon.sprites.front_shiny;
-    } else {
-      imgPK.src = 'img/nao-encontrado.png';
+
+    try {
+        if (dataPokemon.sprites.front_default) {
+            imgPK.src = dataPokemon.sprites.front_default;
+        } else if (dataPokemon.sprites.front_shiny) {
+            imgPK.src = dataPokemon.sprites.front_shiny;
+        } else {
+            imgPK.src = 'img/nao-encontrado.png';
+        }
+
+        imgPK.onerror = () => {
+            imgPK.src = 'img/nao-encontrado.png';
+        };
+
+    } catch (erro) {
+        console.error('Erro ao definir imagem do Pokémon:', erro);
+        if (imgPK) {
+            imgPK.src = 'img/nao-encontrado.png';
+        }
     }
-  
-    // Caso a imagem falhe ao carregar (ex: URL quebrada)
-    imgPK.onerror = () => {
-      imgPK.src = 'img/nao-encontrado.png';
-    };
-  
-  } catch (erro) {
-    console.error('Erro ao definir imagem do Pokémon:', erro);
-    // Fallback total se algo der muito errado
-    if (imgPK) {
-      imgPK.src = 'img/nao-encontrado.png';
-    }
-  }
+
     idPK.innerHTML = `<br>ID:${dataPokemon.id}`;
     nomePK.innerHTML = dataPokemon.name;
     tipo1.innerHTML = dataPokemon.types[0].type.name;
-    if(
-        dataPokemon.types[1] != undefined
-    ){
-    tipo2.innerHTML = dataPokemon.types[1].type.name;}
-    else{
-        tipo2.innerHTML = ""
+
+    if (dataPokemon.types[1] != undefined) {
+        tipo2.innerHTML = dataPokemon.types[1].type.name;
+    } else {
+        tipo2.innerHTML = "";
     }
+
     habiPK.innerHTML = dataPokemon.abilities[0].ability.name;
-    pesoPK.innerHTML =`Peso:${dataPokemon.weight/10}kg`;
-    alturaPK.innerHTML =`Altura:${dataPokemon.height/10}m`;
+    pesoPK.innerHTML = `Peso:${dataPokemon.weight / 10}kg`;
+    alturaPK.innerHTML = `Altura:${dataPokemon.height / 10}m`;
 }
-formPK.addEventListener("submit",(event)=>{
+
+formPK.addEventListener("submit", (event) => {
     event.preventDefault();
     showPokemon(inputPK.value.toLowerCase());
-})
-btnVoltar.addEventListener("click",(e)=>{
-    if(numeroPokedex>1){
+});
+
+btnVoltar.addEventListener("click", (e) => {
+    if (numeroPokedex > 1) {
         numeroPokedex--;
     }
     showPokemon(numeroPokedex);
-})
-btnProximo.addEventListener("click",(e)=>
-{
-if(numeroPokedex<1025){
-    numeroPokedex++;
-}
-showPokemon(numeroPokedex);
-})
+});
 
+btnProximo.addEventListener("click", (e) => {
+    if (numeroPokedex < 1025) {
+        numeroPokedex++;
+    }
+    showPokemon(numeroPokedex);
+});
 
 showPokemon(numeroPokedex);
